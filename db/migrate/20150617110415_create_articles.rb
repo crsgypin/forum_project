639e77d891b4e572c22065777bda2823af2038df
crsgypin <crsgypin@gmail.com>
2015-06-17 19:32:46 +0800

    Add models of article, comment and user_profile

---
+++ db/migrate/20150617110415_create_articles.rb
@@ -0,0 +1,13 @@
+class CreateArticles < ActiveRecord::Migration
+  def change
+    create_table :articles do |t|
+
+    	t.string :title
+    	t.text :content
+    	t.integer :author_id
+    	t.integer :category_id
+
+      t.timestamps null: false
+    end
+  end
+end
---
+++ db/migrate/20150617110707_create_user_profiles.rb
@@ -0,0 +1,16 @@
+class CreateUserProfiles < ActiveRecord::Migration
+  def change
+    create_table :user_profiles do |t|
+
+    	t.stirng :nick_name
+    	t.string :first_name
+    	t.string :last_name
+    	t.string :english_name
+    	t.date :birthdate
+    	t.text :intro
+    	t.integer :user_id
+
+      t.timestamps null: false
+    end
+  end
+end

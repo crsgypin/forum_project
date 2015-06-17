dfdd4fd3fcbb404b82e8eca2907965fb76c3a5b4
crsgypin <crsgypin@gmail.com>
2015-06-18 00:11:59 +0800

    seed cat and article data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,29 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-10.times do |index|
+Category.destroy_all
+Category.create(:id=>1, :name=>"Baseball",:intro=>"Curry wins Labron")
+Category.create(:id=>2, :name=>"Baskball",:intro=>"Ichiro go 3000 hits")
+Category.create(:id=>3, :name=>"Math",:intro=>"Math is beautiful")
+Category.create(:id=>4, :name=>"Computer",:intro=>Faker::Lorem.sentences)
+Category.create(:id=>5, :name=>"Physics",:intro=>Faker::Lorem.sentences)
+Category.create(:id=>6, :name=>"English",:intro=>Faker::Lorem.sentences)
+
+ArticleCategoryship.destroy_all
+Article.destroy_all
+
+6.times do |index|
 	# User.create(:email=>Faker::Internet.email,:username=>Faker::Name.name,:password=>"qwerty1234",:password_confirmation=>"qwerty1234",:confirmed_at=>Time.now)
 
-
+	c = Category.find(index+1)
+	10.times do |i|
+		a = Article.create(:title=>Faker::Name.title,:content=>Faker::Lorem.paragraph)
+		ArticleCategoryship.create(:article=>a,:category=>c)
+	end
 end
 
-Category.destroy_all
-Category.create(:name=>"Baseball",:intro=>"Curry wins Labron")
-Category.create(:name=>"Baskball",:intro=>"Ichiro go 3000 hits")
-Category.create(:name=>"Math",:intro=>"Math is beautiful")
-Category.create(:name=>"Computer",:intro=>Faker::Lorem.sentences)
-Category.create(:name=>"Physics",:intro=>Faker::Lorem.sentences)
-Category.create(:name=>"English",:intro=>Faker::Lorem.sentences)
+
+
+
 
 

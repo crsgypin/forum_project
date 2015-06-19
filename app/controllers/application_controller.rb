6f5f27880b8b4621eaa4cd0e46ed71ce7aaf509d
crsgypin <crsgypin@gmail.com>
2015-06-19 13:29:18 +0800

    devise view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,18 @@
 class ApplicationController < ActionController::Base
+	before_action :set_active_category
+
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+
+protected
+	
+	def set_active_category
+		active_id = (params[:category_id])? params[:category_id] : 1
+		@active_category = Category.find(active_id)
+	end
+
+
 end
--- app/controllers/forums_controller.rb
+++ app/controllers/forums_controller.rb
@@ -2,10 +2,9 @@ class ForumsController < ApplicationController
 	before_action :authenticate_user!
 
 	def index
-		active_id = (params[:category_id])? params[:category_id] : 1
+
 		@per = 10
 		@page = params[:page].to_i
-		@active_category = Category.find(active_id)
 		@active_articles = @active_category.articles.page(@page).per(@per)
 
 

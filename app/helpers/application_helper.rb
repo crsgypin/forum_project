a78200b03f5fa1730d7e7c8563b4247ef4154662
ChiuGypin <crsgypin@gmail.com>
2015-06-22 16:03:49 +0800

    complete gravatar feature

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 	def avatar_url(user)
-		gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-		"http://gravatar.com/avatar/#{gravatar_id}.png"
+		gravatar_id = Digest::MD5.hexdigest(user.email)
+		"http://gravatar.com/avatar/#{gravatar_id}"
 	end
 
 end

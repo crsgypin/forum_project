840c856de6856d3605ace974eb599a5a25dd8dd1
ChiuGypin <crsgypin@gmail.com>
2015-07-02 19:29:34 +0800

    Ajax comment: Update comment by ajax separate to form and row

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,10 @@
 class CommentsController < ApplicationController
 	before_action :authenticate_user!, :find_article_and_comments
 
+	def new
+		@comment = Comment.new
+	end
+
 	def create		
 		if current_user
 			@comment = Comment.new(params_comment)
@@ -9,22 +13,20 @@ class CommentsController < ApplicationController
 			if @comment.save		
 				# @comment.article.touch(:last_comment_at)
 				
-				@comment = Comment.new					
+				@new_comment = Comment.new					
 				@notice = 'your comment has been successful posted'
 			else
 				@alert = @comment.errors.full_messages.join(' ').html_safe
+				render 'new'
 			end
 
-			refresh
 		else
 			redirect_to new_session(:user)
 		end
 	end
 
 	def edit
-
 		@comment = Comment.find(params[:id])
-		refresh
 	end
 
 	def update
@@ -32,21 +34,19 @@ class CommentsController < ApplicationController
 		@comment.update(params_comment)
 		if @comment.save
 			@notice = "your comment has been successfully updated"
-			@comment = Comment.new
+			@new_comment = Comment.new
 
 		else
 			@alert = @comment.errors.full_messages.join(' ').html_safe
 		end
-		refresh
 	end
 
 
 	def destroy
 		@comment = Comment.find(params[:id])
 		@comment.destroy
-		@comment = Comment.new
+		@new_comment = Comment.new
 		@notice = "You comment has been successfully deleted"
-		refresh
 	end
 
 private
